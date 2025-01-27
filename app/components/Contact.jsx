899ef1fc6ef1b6626e80943945e2a7bd1b5b2631
Balı Tabur <webtasarim.0323@gmail.com>
2025-01-27 14:57:47 +0300

    add:contact section

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -1,15 +1,24 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Contact = () => {
   return (
-    <section className='relative bg-[url("/bg-contact.jpg")] flex flex-col items-start gap-10 bg-cover bg-center bg-no-repeat h-[70vh] mb-20'>
-      <div className='absolute inset-0 bg-black/50'></div>
-
-      <Image 
-      alt='contact-image'
+    <section className='relative bg-[url("/bg-contact.jpg")] pt-20 pl-20 flex flex-col items-start gap-10 bg-cover bg-center bg-no-repeat h-[90vh] mb-20'>
+      <div className='absolute inset-0 bg-black/50 z-0'></div>
       
-      />
+      <div className='relative z-10 flex flex-col lg:gap-10 gap-5'>
+        <Image 
+          alt='contact-image'
+          src={'/contact-image.jpg'}
+          width={1800}
+          height={900}
+          className='lg:w-2/3 w-[80%] object-contain'
+        />
+        <h2 className='text-3xl font-semibold text-white'>Bir Fikriniz Mi Var ?</h2>
+        <p className='text-xl text-white w-[90%]'>Petrolden inşaata iştiraklerimiz olan alanlarla ilgili bize ulaşabilirsiniz…</p>
+        <Link href={"/iletişim"} className='py-2 w-32 text-center bg-[#fdc133] text-white'>İletişim</Link>
+      </div>
 
     </section>
   )
